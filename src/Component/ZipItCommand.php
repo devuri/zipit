a651a2ebdd4de26dbbe9ed134ef60da371ce58aa
devuri <devuri@users.noreply.github.com>
2024-11-01 09:45:08 -0500

    feat!: initial

---
+++ src/Component/ZipItCommand.php
@@ -0,0 +1,143 @@
+<?php
+
+/*
+ * This file is part of the WPframework package.
+ *
+ * (c) Uriel Wilson
+ *
+ * The full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Urisoft;
+
+use RecursiveDirectoryIterator;
+use RecursiveIteratorIterator;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Helper\ProgressBar;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
+use Symfony\Component\Filesystem\Filesystem;
+use ZipArchive;
+
+class ZipItCommand extends Command
+{
+    protected static $defaultName = 'zipit';
+
+    protected function configure(): void
+    {
+        $this
+            ->setDescription('Creates a zip file based on the configuration in .zipit-conf.php')
+            ->addArgument('output', InputArgument::REQUIRED, 'The name of the output zip file (must end with .zip)')
+            ->addArgument('config', InputArgument::OPTIONAL, 'Path to the configuration file (must be .zipit-conf.php)', getcwd() . '/.zipit-conf.php');
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $io = new SymfonyStyle($input, $output);
+
+        $outputFileName = $input->getArgument('output');
+        $configFilePath = $input->getArgument('config');
+
+        if ('zip' !== pathinfo($outputFileName, PATHINFO_EXTENSION)) {
+            $io->error("The output file name must have a .zip extension.");
+
+            return Command::FAILURE;
+        }
+
+        if ('.zipit-conf.php' !== basename($configFilePath)) {
+            $io->error("The configuration file must be named .zipit-conf.php.");
+
+            return Command::FAILURE;
+        }
+
+        if ( ! file_exists($configFilePath)) {
+            $io->error("Configuration file .zipit-conf.php not found at $configFilePath.");
+
+            return Command::FAILURE;
+        }
+
+        $config = require $configFilePath;
+        if ( ! \is_array($config) || ! isset($config['files'], $config['baseDir'], $config['exclude']) || ! \is_array($config['files']) || ! \is_array($config['exclude'])) {
+            $io->error("Invalid configuration file. The .zipit-conf.php file must return an array with 'baseDir', 'files', and 'exclude' keys.");
+
+            return Command::FAILURE;
+        }
+
+        $baseDir = realpath($config['baseDir']);
+        $files = $config['files'];
+        $excludes = array_map('realpath', array_map(fn ($file) => $baseDir . DIRECTORY_SEPARATOR . $file, $config['exclude']));
+        $filesystem = new Filesystem();
+
+        if (file_exists($outputFileName)) {
+            $filesystem->remove($outputFileName);
+        }
+
+        $zip = new ZipArchive();
+        if (true !== $zip->open($outputFileName, ZipArchive::CREATE)) {
+            $io->error("Failed to create zip file.");
+
+            return Command::FAILURE;
+        }
+
+        $io->title("Creating Zip Archive");
+        $progressBar = new ProgressBar($output, \count($files));
+        $progressBar->start();
+
+        foreach ($files as $file) {
+            $filePath = realpath($baseDir . DIRECTORY_SEPARATOR . $file);
+            if ( ! $filePath || ! $filesystem->exists($filePath)) {
+                $io->warning("File or directory '$file' does not exist.");
+
+                continue;
+            }
+
+            if ($this->isExcluded($filePath, $excludes)) {
+                $io->note("Skipping excluded file or directory: '$file'");
+
+                continue;
+            }
+
+            $this->addFileToZip($zip, $filePath, $baseDir, $excludes);
+            $progressBar->advance();
+        }
+
+        $progressBar->finish();
+        $zip->close();
+        $io->success("Zip file created successfully.");
+
+        return Command::SUCCESS;
+    }
+
+    private function isExcluded($filePath, $excludes)
+    {
+        foreach ($excludes as $exclude) {
+            if (0 === strpos($filePath, $exclude)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    private function addFileToZip(ZipArchive $zip, $filePath, $basePath, $excludes): void
+    {
+        if (is_dir($filePath)) {
+            $iterator = new RecursiveIteratorIterator(
+                new RecursiveDirectoryIterator($filePath, RecursiveDirectoryIterator::SKIP_DOTS)
+            );
+            foreach ($iterator as $item) {
+                if ($this->isExcluded($item->getPathname(), $excludes)) {
+                    continue;
+                }
+                $relativePath = substr($item->getPathname(), \strlen($basePath) + 1);
+                $zip->addFile($item->getPathname(), $relativePath);
+            }
+        } else {
+            $relativePath = substr($filePath, \strlen($basePath) + 1);
+            $zip->addFile($filePath, $relativePath);
+        }
+    }
+}
