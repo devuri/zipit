c7f2c0272903dbfe940d2233d42c5a0acfff1a22
devuri <devuri@users.noreply.github.com>
2025-02-15 10:09:07 -0500

    test: add test file and test config file

---
+++ tests/.zipit-conf.php
@@ -0,0 +1,21 @@
+<?php
+
+// return [
+//     'baseDir' => __DIR__,
+//     'files' => [
+//         'bootstrap.php',
+//         'stubs.php',
+//         'Unit',
+//     ],
+// ];
+
+return [
+    'baseDir' => __DIR__,
+    'files' => [
+        'bootstrap.php',
+        'stubs.php',
+        'Unit',
+    ],
+    'outputDir' => __DIR__ . '/build/trunk',
+    'outputFile' => 'unit-test.zip',
+];
